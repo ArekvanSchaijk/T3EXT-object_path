c23d2ceee9ea95056be334a92733bb52bc5e0ae2
Arek van Schaijk <info@ucreation.nl>
2015-08-27 20:14:51 +0200

    Updated extension manager configuration

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,39 +1,28 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "object_path".
-#
-# Auto generated 27-08-2015 20:10
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
-
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = array (
 	'title' => 'Typoscript Object Path from Content Element',
 	'description' => 'Front-end plugin where u can load a Typoscript Object Path',
 	'category' => 'plugin',
-	'shy' => 0,
 	'version' => '2.1.0',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'TYPO3_version' => '4.5-0.0.0',
-	'PHP_version' => '',
-	'module' => '',
 	'state' => 'stable',
-	'uploadfolder' => 0,
+	'uploadfolder' => FALSE,
 	'createDirs' => '',
-	'modify_tables' => '',
-	'clearcacheonload' => 0,
-	'lockType' => '',
+	'clearcacheonload' => FALSE,
 	'author' => 'Arek van Schaijk',
 	'author_email' => 'info@ucreation.nl',
 	'author_company' => 'Ucreation',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'typo3' => '6.0.0-6.2.99',
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
 );
-
-?>
